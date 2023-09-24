ab16efc3375b3a6aa8b60a669a6be04a5b4cabde
Digontha <digonthodj@gmail.com>
2023-09-24 20:10:41 +0600

    first commit

---
+++ src/Components/Header/Nav.jsx
@@ -0,0 +1,49 @@
+import { NavLink } from "react-router-dom";
+
+
+const Nav = () => {
+    return (
+        <div className="flex justify-between items-center">
+            <div>
+                <img src="/src/assets/Logo.png" alt="" />
+            </div>
+
+            <nav>
+                <ul className="flex gap-5 font-bold">
+                    <li>
+                        <NavLink
+                            to="/"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-red-500 underline" : ""
+                            }
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/messages"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-red-500 underline" : ""
+                            }
+                        >
+                           Donation
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/messages"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-red-500 underline" : ""
+                            }
+                        >
+                           Statistics
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Nav;
